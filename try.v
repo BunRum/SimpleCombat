61c836b26eda526933300af9b7cecc65d05aaf65
bun <bleh>
2024-08-04 18:32:53 +0000

    Aug 4, 2024, 11:32 AM

--- try.v
+++ try.v
@@ -16,7 +16,7 @@ fn combine(mut tbl1 []string, tbl2 []string) {
 }
 
 fn rec(dir string) []string  {
-	mut files := os.ls(dir) or {[]}
+	mut files := []string
 	// println(files)
 	for directory in os.ls(dir) or {[]} {
 		name := '${dir}/${directory}'
