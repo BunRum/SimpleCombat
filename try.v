a00e63ed7ffcd4d295a8636c44610ddfb8061f86
bun <bleh>
2024-08-04 18:47:39 +0000

    Aug 4, 2024, 11:47 AM

--- try.v
+++ try.v
@@ -6,7 +6,7 @@ import os
 
 fn main() {
 	for file in rec("./src/characters") {
-		filename := file.all_before(".")
+		filename := file.all_before("server")
 		if os.file_ext(file) == ".luau" {
 			println(filename)
 		}
