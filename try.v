bbe075400fe83a644b90a3c8a73aa2f963d1db27
bun <bleh>
2024-08-04 18:24:22 +0000

    Aug 4, 2024, 11:24 AM

--- try.v
+++ try.v
@@ -9,7 +9,7 @@ fn main() {
 	print(rec("./"))
 }
 
-fn combine(mut tbl1 [], tbl2 []) {
+fn combine(mut tbl1 []string, tbl2 []string) {
 	for variable in tbl2 {
 		tbl1.insert(tbl1.len, variable)
 	}
@@ -19,7 +19,7 @@ fn rec(dir string) []string  {
 	mut files := os.ls(dir) or {[]}
 	for directory in files {
 		if os.is_dir(directory) {
-			combine(files, rec(directory))
+			combine(mut&files, rec(directory))
 		}
 	}
 	return files
